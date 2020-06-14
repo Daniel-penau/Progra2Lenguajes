0c4f787062d4b9a27ccbe585b883c0bacad47234
Dydrey <57191864+Dydrey@users.noreply.github.com>
2020-06-14 00:27:55 -0600

    Update bipartito.pl
    [+] Camino
    [+]Máximo
    [+]Mínimo
    [+]Largo
    [+]EliminaRep
    [+]

--- bipartito.pl
+++ bipartito.pl
@@ -33,3 +33,39 @@ removeL([], _, []).
 removeL([H|T], L2, R):- member(H, L2), !, removeL(T, L2, R). 
 removeL([H|T], L2, [H|R]):- removeL(T, L2, R).
 
+
+%Busca el un camino en un grafo G desde un nodo A hasta un nodo B
+%Su resultado queda en Path
+camino(G,A,B,Path) :-atravesar(G,A,B,[A],Q),reverse(Q,Path).
+atravesar(G,A,B,P,[B|P]) :- conectado(A,B,G).
+atravesar(G,A,B,Visitado,Camino) :-conectado(A,C,G), C \== B,
+    \+member(C,Visitado), atravesar(G,C,B,[C|Visitado],Camino).  
+%Verifica que el nodo X y Y esten conectados, siendo miembros de
+%la lista de las aristas
+conectado(X,Y,G) :- member([X,Y],G);member([Y,X],G).
+%Ejemplo de prueba:
+%camino([[a,b],[a,e],[c,b],[c,d],[e,d],[d,f],[e,b]],a,e,C)
+
+%Saca el maximo entre dos numeros X y Y
+maximo(X,Y,X):- X >= Y.
+maximo(X,Y,Y):- X < Y.
+%Ejemplo de prueba:
+%maximo(9,13,C)
+
+%Saca el minimo entre dos numeros X y Y
+minimo(X,Y,X):- X =< Y.
+minimo(X,Y,Y):- X>Y.
+%Ejemplo de prueba:
+%minimo(9,3,C)
+
+%Saca la cantidad de aristas que hay entre un nodo A y un nodo B
+%en un grafo G
+largo(G,A,B,L) :- camino(G,A,B,P), length(P,T), L is T-1.
+%Ejemplo de prueba:
+%largo([[a,b],[a,e],[c,b],[c,d],[e,d],[d,f],[e,b]],a,e,L)
+
+%Dada una lista de aristas G, devuelve una lista con los nodos
+eliminaRep(G,L) :- flatten(G,P), sort(P,L).
+%Ejemplo de prueba:
+%eliminaRep([[a,b],[a,e],[c,b],[c,d],[e,d],[d,f],[e,b]],L)
+
